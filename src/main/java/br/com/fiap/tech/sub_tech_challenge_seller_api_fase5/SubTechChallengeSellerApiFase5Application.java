e399b657d5210a5b1e4b2e4dcf53e89e8df1ce6f
Lucas Bortolo <lucasbortolo3@gmail.com>
2025-03-04 13:25:32 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_seller_api_fase5/SubTechChallengeSellerApiFase5Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_seller_api_fase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTechChallengeSellerApiFase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubTechChallengeSellerApiFase5Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/tech/sub_tech_challenge_seller_api_fase5/SubTechChallengeSellerApiFase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_seller_api_fase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTechChallengeSellerApiFase5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
